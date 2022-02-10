9c74294342cf2ea74680c4a450a95b829b867a08
rico <rikardoan@gmail.com>
2022-02-10 14:21:32 -0500

    fixed login-home

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -6,6 +6,9 @@ import BillsInput from "../components/BillsInput";
 import BillsList from "../components/BillsList";
 import BudgetInput from "../components/BudgetInput";
 import Display from "../components/Display";
+import { LOGIN } from '../utils/mutations';
+import { Link } from 'react-router-dom';
+import Auth from '../utils/auth';
 
 // const Home = () => {
 //   return (
@@ -22,14 +25,46 @@ import Display from "../components/Display";
 // };
 
 const Home = () => {
-  return (
-    <main>
-    <BudgetInput />
-    <BillsInput />
-    <BillsList />
-    <Display />
-    </main>
-  )
-}
+
+  if (Auth.loggedIn()) {
+    return (
+      <main>
+       <BudgetInput />
+      <BillsInput />
+      <BillsList />
+      <Display /> 
+      </main>
+    );
+  } else {
+    return (<>
+     MORE MONEY MORE PROBLEMS
+        
+        <br></br>
+        Animation logo intro
+        <br></br>
+        Loading
+        <br></br>
+      <ul className="flex-row">
+
+       
+
+
+        <li>
+          <Link to="/signup">
+            Signup
+          </Link>
+        </li>
+        <li className="mx-1">
+          <Link to="/login">
+            Login
+          </Link>
+        </li>
+      </ul>
+      </>
+    );
+  }
+
+ 
+};
 
 export default Home;
